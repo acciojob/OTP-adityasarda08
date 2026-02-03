d872fb0af4a229755a2ccadaabfcadd75a152780
adityasarda08 <142217693+adityasarda08@users.noreply.github.com>
2026-02-03 18:52:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-//your JS code here. If required.
 const inputs = document.querySelectorAll(".code");
 
+// Cypress expects first input focused
 inputs[0].focus();
 
 inputs.forEach((input, index) => {
   input.addEventListener("keydown", (e) => {
+
+    // Allow only digits
     if (e.key >= "0" && e.key <= "9") {
       input.value = "";
       setTimeout(() => {
         if (inputs[index + 1]) {
           inputs[index + 1].focus();
         }
-      }, 10);
+      }, 0);
     }
 
+    // Backspace behavior
     if (e.key === "Backspace") {
-      input.value = "";
-      setTimeout(() => {
+      if (input.value === "") {
         if (inputs[index - 1]) {
           inputs[index - 1].focus();
         }
-      }, 10);
+      } else {
+        input.value = "";
+      }
     }
   });
 });
